d58d1697b21983fa2619bee07fa6475ab2a1a482
Nastaran <nastaranadib1383@gmail.com>
2025-08-24 20:16:05 +0330

    solving bugs

--- exchange/views.py
+++ exchange/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Exchange, ExchangeAddress, ExchangePhone, ExchangeStaff
-from .models.staff import Staff
+from .models.exchange import Exchange, ExchangeAddress, ExchangePhone
+from .models.staff import Staff , StaffPhone
+from .models.currency import CurrencyPrice , CurrencyType
+from .models.debt import Debt
+from .models.relations import ExchangeCurrency , ExchangeStaff
 
 
 
