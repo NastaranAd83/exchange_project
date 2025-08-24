c36b4fdd267b454abd4d5e7147782e916a39de52
Nastaran <nastaranadib1383@gmail.com>
2025-08-24 18:28:40 +0330

    working on exchange and staff class and the relations

---
+++ exchange/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Exchange, ExchangeAddress, ExchangePhone, ExchangeStaff
+from .models.staff import Staff
+
+
+
+
+
+
+
+
+# Create your views here.
